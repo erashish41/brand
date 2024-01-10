ffe9a8bb23cf03c3334d159d0ede885e82bd7c85
Ashish Bhardwaj <erasish41@gmail.com>
2024-01-10 18:58:12 +0530

    new update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+const HeroSection = () => {
+    return (
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVES THE BEST</h1>
+                <p> dskjhfkjshdjfhsfjkhskjhfjkdshfkjdshkjhsdkjhjksdhfjsdhjfk</p>
+
+                <div className="hero-button">
+                    <button>Shop Now</button>
+                    <button>Category</button>
+                </div>
+                
+                <div className="Shopping">
+                    <p> also Availble</p>
+
+                    <div className="brand-icons">
+                        <img src="images/shoes_pic.png" alt="nikelogo"></img>
+                    </div>
+                </div>
+
+               
+            </div>
+            <div className="hero-image"></div>
+        </main>
+    )
+
+}
+
+export default HeroSection;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+const Navigation = () => {
+
+    return (
+        <nav>
+        <div className="logo">
+            <img src="/images/Nike logo.png" alt="logo"/>
+        </div>
+
+        <ul>
+            <li href="#">New & Featured</li>
+            <li href="#">Men</li>
+            <li href="#">women</li>
+            <li href="#">Kids</li>
+            <li href="#">Sale</li>
+            <li href="#">Coustomise</li>
+            <li href="#">SNKRS</li>
+        </ul>
+        <button>Login </button>
+    </nav>
+    )
+}
+
+export default Navigation;
